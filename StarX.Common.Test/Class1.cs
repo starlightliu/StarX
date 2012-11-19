4da596184d7bdeca6806ee0c09004029e3b0d8de
starlightliu <starligthliu@gmail.com>
2012-11-19 19:35:28 +0800

    use common.logging with nlog

--- StarX.Common.Test/Class1.cs
+++ StarX.Common.Test/Class1.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Common.Logging;
+using NUnit.Framework;
 
 namespace StarX.Common.Test
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void LogTest()
+        {
+            var log = LogManager.GetCurrentClassLogger();
+            log.Error("test");
+        }
     }
 }
