3845de305929848d62061066d38607bcec28fba9
starlightliu <starligthliu@gmail.com>
2012-11-19 22:46:55 +0800

    add Quartz and remove useless log modules

--- StarX.Common.Test/Class1.cs
+++ StarX.Common.Test/Class1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Common.Logging;
+//using Common.Logging;
 using NUnit.Framework;
+using Quartz;
+using Quartz.Impl;
 
 namespace StarX.Common.Test
 {
@@ -13,8 +15,14 @@ namespace StarX.Common.Test
         [Test]
         public void LogTest()
         {
-            var log = LogManager.GetCurrentClassLogger();
-            log.Error("test");
+            //var log = LogManager.GetCurrentClassLogger();
+            //log.Trace("Trace");
+            //log.Debug("Debug");
+            //log.Info("Info");
+            //log.Warn("Warn");
+            //log.Error("Error");
+            //log.Fatal("Fatal");
         }
+
     }
 }
